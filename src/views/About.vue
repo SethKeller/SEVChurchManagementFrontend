ea6b08d7e4f4e5e53f69c23c73ba358c31853fcf
Logan Giese <logan.giese@eagles.oc.edu>
2021-02-10 14:51:03 -0700

    Update package.json and add BootstrapVue
    Node dependencies/plugins updated and BootstrapVue added to the project

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <b-alert style="width:400px;margin:auto" show dismissible variant="warning">
+      Alert: This is a bootstrap alert!
+    </b-alert>
   </div>
 </template>
