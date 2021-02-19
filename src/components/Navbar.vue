ec8ab4b57b083653a2a9bc35f9857905b24abd29
Logan Giese <logan.giese@eagles.oc.edu>
2021-02-19 11:58:19 -0600

    Add basic landing page

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="navbar bg-dark text-light px-4 py-3 mb-3">
+  <div class="navbar bg-dark text-light px-4 py-3 mb-4">
     <b-nav pills>
       <h3 class="my-auto pr-4">Wilshire Church of Christ</h3>
       <!-- TODO: change this to current congregation name -->
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1 class="pb-3">Welcome to Wilshire Church of Christ!</h1>
+    <img alt="Wilshire logo" src="../assets/wilshire-logo.jpg" width="350px"/>
+    <h5 class="py-4">This system allows you to manage your member information and view the church member directory</h5>
+    <h4>Please select an option from the menu above to get started</h4>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    
   }
 };
 </script>
