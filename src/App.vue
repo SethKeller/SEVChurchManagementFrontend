0a05f4bd38693a64e6d3ecd741891a910d58d91d
Logan Giese <logan.giese@eagles.oc.edu>
2021-02-19 11:54:27 -0600

    Add Wilshire logo and styling updates

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <Navbar />
-    <router-view />
+    <div id="viewport">
+      <router-view />
+    </div>
     <!-- Main app view -->
   </div>
 </template>
@@ -24,18 +26,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
