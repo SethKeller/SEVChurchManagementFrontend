d8d6589ccb56734d625beb2ac399b758016250d8
ocTrung <t.nguyenvo@eagles.oc.edu>
2021-02-23 15:38:32 -0600

    view and edit congregation

---
+++ src/views/Congregation.vue
@@ -0,0 +1,95 @@
+<template>
+  <div>
+    <b-form @submit="onSubmit" v-if="show">
+      <b-form-group id="input-group-name" label="Congregation Name:" label-for="input-name">
+        <b-form-input
+          id="input-name"
+          v-model="form.name"
+          placeholder="Enter name"
+          required
+        ></b-form-input>
+      </b-form-group>
+      
+      <b-form-group id="input-group-address" label="Congregation Address:" label-for="input-address">
+        <b-form-input
+          id="input-address"
+          v-model="form.address"
+          placeholder="Enter name"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-phone" label="Congregation Phone:" label-for="input-phone">
+        <b-form-input
+          id="input-phone"
+          v-model="form.phone"
+          placeholder="Enter phone number"
+          required
+        ></b-form-input>
+      </b-form-group>
+      
+      <b-button type="submit" variant="primary">Submit</b-button>
+      </b-form>
+  </div>
+</template>
+
+
+<script>
+  import CongregationServices from '../services/CongregationServices.js';
+  export default {
+    components: {},
+      data() {
+        return {
+          form: {
+            name: "",
+            address: "",
+            phone: "",
+          },
+          congregation: [],
+          show: true,
+        };
+      },
+      created() {
+        // need a way to select MY church; current API returns either a list of congregations or you can request a congregation by ID
+        // defaulting congregation with id of 1 since we are only working with one church right now
+        // maybe before this page, we should ask the user to choose a congregation
+        this.getCongregation(1);
+      },
+      methods: {
+        getCongregation(id) {
+          CongregationServices.getCongregation(id)
+            .then(response => {
+              this.congregation = response.data;
+
+              this.form.name = this.congregation.Name;
+              this.form.address = this.congregation.Address;
+              this.form.phone = this.congregation.Phone;
+            })
+            .catch(error => {
+              this.message = error.response.data.message;
+            })
+        },
+        onSubmit() {
+          let newCongregation = {
+            "Name": this.form.name,
+            "Address": this.form.address,
+            "Phone": this.form.phone
+          }
+          this.updateCongregation(1, newCongregation);
+        },
+        updateCongregation(id, congregation) {
+          CongregationServices.updateCongregation(id, congregation)
+            .then(() => {
+              this.$router.push('congregation');
+            }) 
+            .catch((error) => {
+              this.message = error.response;
+            })
+        }
+      }
+  };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
