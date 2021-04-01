7e4dbd3e62c7f79f20e473925fc05ca420516332
ocTrung <t.nguyenvo@eagles.oc.edu>
2021-03-31 21:28:39 -0500

    change homepage if logged in

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,37 @@
 <template>
   <div class="home">
-    <h1 class="pb-3">Welcome to Wilshire Church of Christ!</h1>
-    <img alt="Wilshire logo" src="../assets/wilshire-logo.jpg" width="350px" />
-    <h5 class="py-4">
-      This system allows you to manage your member information and view the
-      church member directory
-    </h5>
-    <h4>Please select an option from the menu above to get started</h4>
+    <h1 class="m-3">Welcome!</h1>
+    <img class="m-3" alt="Wilshire logo" src="../assets/wilshire-logo.jpg" width="350px" />
+    <div v-show="loggedIn">
+      <h5 class="py-4">
+        This system allows you to manage your member information and view the
+        church member directory
+      </h5>
+      <h4>Please select an option from the menu above to get started</h4>
+    </div>
+    <div class="content" align-self="center" v-show="!loggedIn">
+      <h4>Announcements</h4>
+      <p>
+        We are excited to return to in person worship beginning Sunday, February
+        28th at 9:30am. We encourage everyone to practice safe distancing and
+        wear a facemask if possible. You can also join our worship via
+        Livestream by subscribing to the Wilshire Church of Christ YouTube
+        channel.
+      </p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "Home",
-  components: {}
+  components: {},
+  computed: {
+    loggedIn() {
+      return this.$store.state.auth.status.loggedIn;
+    }
+  }
 };
 </script>
+
+<style scoped></style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -69,7 +69,7 @@ export default {
       if (this.user.email && this.user.password) {
         this.$store.dispatch("auth/login", this.user).then(
           () => {
-            this.$router.push("home");
+            this.$router.push("Home");
           },
           error => {
             this.loading = false;
