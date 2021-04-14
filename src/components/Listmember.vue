b0675199694fa9539e65e9c442f5cdb06d1b2788
SethKeller <seth.m.keller@eagles.oc.edu>
2021-04-14 16:51:58 -0500

    Left justified text, placed buttons in the same row

--- src/components/Listmember.vue
+++ src/components/Listmember.vue
@@ -1,12 +1,13 @@
 <template>
     <b-card no-body class = "overflow-hidden shadow" style="min-width: 800px">
         <b-row no-gutters>
-            <b-col md="20">
-                <b-card-body :title="member.FirstName + ' ' + member.LastName">
-                <b-button href="#" variant="primary">Edit</b-button>
-                <b-button href="#" variant="primary">Delete</b-button>
-                </b-card-body>
+            <b-col md="6">
+                <b-card-body :title="member.FirstName + ' ' + member.LastName" class = "text-left"> </b-card-body>               
             </b-col>  
+            <b-col md="6" class="mt-3">
+                <b-button href="#" variant="primary" class = "mr-1">Edit</b-button>
+                <b-button href="#" variant="primary">Delete</b-button>
+                </b-col>
         </b-row>
     </b-card>              
 </template>
