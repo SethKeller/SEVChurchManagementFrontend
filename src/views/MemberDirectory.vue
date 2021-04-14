69a745b4a5d21a8f6e13ca1ee2d5111d8cdc8f7b
Logan Giese <logan.giese@eagles.oc.edu>
2021-04-14 16:58:41 -0500

    Add family mode to directory page
    Now gets the list of families and has family mode switching functionality (but no family info cards yet)

--- src/views/MemberDirectory.vue
+++ src/views/MemberDirectory.vue
@@ -3,33 +3,45 @@
     <h1 class="pb-3">Church Directory</h1>
     <h4>{{ message }}</h4>
     
+    <b-form-checkbox switch size="lg" v-model="familyMode">Family Mode</b-form-checkbox>
+    <br/>
+    
     <Search v-on:search-submitted="doSearch" class="m-auto pb-3" style="width:340px" />
-    <MemberInfo
-      v-for="member in members"
-      :key="member.LASTNAME"
-      :member="member"
-      class="m-2 d-md-inline-block"
-    />
-
-    <!-- TODO: footer with pages OR infiniteload? -->
+    <div v-if="!familyMode">
+      <MemberInfo
+        v-for="member in members"
+        :key="member.LASTNAME"
+        :member="member"
+        class="m-2 d-md-inline-block"
+      />
+    </div>
+    <div v-if="familyMode">
+      <h2>FAMILY DIRECTORY GOES HERE</h2>
+    </div>
+    
   </div>
 </template>
 
 <script>
 import MemberInfo from "@/components/MemberInfo.vue";
+//import FamilyDirectoryInfo from "@/components/FamilyDirectoryInfo.vue";
 import MemberListServices from "@/services/MemberListServices.js";
+import FamilyMemberServices from "@/services/FamilyMemberServices.js";
 import Search from "@/components/Search.vue";
 
 export default {
   name: "MemberDirectory",
   components: {
     MemberInfo,
+    //FamilyDirectoryInfo,
     Search
   },
   props: {},
   data() {
     return {
+      familyMode: false,
       members: [],
+      families: [],
       message: "Loading...",
       searchQuery: ""
     };
@@ -46,17 +58,33 @@ export default {
           this.message = error.response.data.message;
         });
     },
+    getFamilyList() {
+      // Load full family list
+      FamilyMemberServices.getFamilies()
+        .then(response => {
+          this.families = response.data;
+          this.message = "";
+        })
+        .catch(error => {
+          this.message = error.response.data.message;
+        });
+    },
     doSearch(members) {
       // Get member list from search component
       this.members = members;
+      // TODO - get family list from search-returned member list?
     }
   },
   created() {
     // Get search query from URL
     if (this.$route.query.search != undefined && this.$route.query.search != "")
       this.searchQuery = this.$route.query.search;
+    
     // Get member list from backend API
     this.getMemberList();
+    
+    // Get family list from backend API
+    this.getFamilyList();
   }
 };
 </script>
